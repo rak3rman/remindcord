7f5caafbc62d64db086cc162aee88d81fbe21774
rak3rman <rak3rman@gmail.com>
2021-11-17 13:00:15 -0600

    Updated README and removed API handlers

--- README.md
+++ README.md
@@ -1,35 +1,27 @@
-# Particlecord
-[![Build Status](https://travis-ci.org/RAK3RMAN/particlecord.svg?branch=main)](https://travis-ci.org/RAK3RMAN/particlecord)
+# Remindcord
+[![Build Status](https://travis-ci.org/RAK3RMAN/remindcord.svg?branch=main)](https://travis-ci.org/RAK3RMAN/remindcord)
 ![Language](https://img.shields.io/badge/language-Node.js-informational.svg?style=flat)
 
-A Discord notification bot that monitors Particle.io events using Node.js
-
-![Home UI](demo.png)
+A Discord bot that sends a repeatable message on a defined interval
 
 ## Purpose
-Particlecord listens to the data stream from Particle.io devices and notifies the user when a particular event occurs.
-Think of Particlecord as an "API middleman" that filters a real-time data stream from Particle.io and sends smart push notifications through Discord.
-Devices can also be managed using the Discord bot by setting friendly names, changing alert frequencies, and outputting the status of any device.
-Particlecord can also easily adapt to your project by modifying the API webhooks in app.js.
 
 ## Install
 As easy as 1, 2, 3.
-1. Clone the repo and enter the directory: ``git clone https://github.com/rak3rman/particlecord.git && cd particlecord``
+1. Clone the repo and enter the directory: ``git clone https://github.com/rak3rman/remindcord.git && cd remindcord``
 2. Install packages: ``npm install``
 2. Run project: ``npm run start``
 
 ## Usage
 ### Configuration
-After the first run of particlecord, a config file will be created in the config folder with path ``/config.json``.
+After the first run of Remindcord, a config file will be created in the config folder with path ``/config.json``.
 This file stores all the environment variables needed for the project, which can be edited when the instance is not running.
 The config file will be populated with the following default values:
-- ``"api_port": 3000`` Port where the api webserver will accept incoming connections, of type int
-- ``"webhook_secret": "random_string_here"`` A random string generated server side for the Discord bot.
 - ``"discord_bot_token": "random_string_generated_here"``
 - ``"discord_bot_channel": "discord_channel_here"``
 - ``"discord_bot_prefix": "!"`` The character that the Discord bot listens to. ! is the default, so !help will display the commands.
 
-**NOTE:** Make sure to stop the instance of particlecord before changing any of these values. If the file is modified while an instance is active, the changes will be overridden.
+**NOTE:** Make sure to stop the instance of Remindcord before changing any of these values. If the file is modified while an instance is active, the changes will be overridden.
 
 ### Running the project
 The npm package supports multiple ways to run the project.
@@ -39,13 +31,7 @@ The npm package supports multiple ways to run the project.
 
 Use ``^C`` to exit any of these instances. Currently, there are no exit commands or words.
 
-### Particlecord commands
-- ``!help`` Displays all Particlecord commands
-- ``!status`` Returns more details from the last alert
-- ``!devices`` Returns all known devices
-- ``!details <device_id>`` Returns all details for a Particle device_id
-- ``!alert_freq <device_id> <freq_in_min>`` Sets the alert frequency in terms of minutes for a device, use 0 for verbose alerts
-- ``!name <device_id> <friendly_name>`` Changes the friendly name for a Particle device_id
+### Remindcord commands
 
 ### Development
 The framework behind this project is not new in the slightest. In fact, [many articles](https://www.section.io/engineering-education/discord-bot-node/) describe how to make a Discord bot using Node.js and explain the basics better than I can. If you'd like to make a Discord bot yourself I would highly suggest checking out these resources.
@@ -56,4 +42,4 @@ The framework behind this project is not new in the slightest. In fact, [many ar
 *Individual contributions are listed on most functions*
 
 ## License
-This project (particlecord) is protected by the Mozilla Public License 2.0 as disclosed in the [LICENSE](https://github.com/rak3rman/particlecord/blob/main/LICENSE). Adherence to the policies and terms listed is required.
+This project (Remindcord) is protected by the Mozilla Public License 2.0 as disclosed in the [LICENSE](https://github.com/rak3rman/remindcord/blob/main/LICENSE). Adherence to the policies and terms listed is required.
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
-App/Filename : particlecord/app.js
+App/Filename : remindcord/app.js
 Description  : Initializes nodejs
 Author       : RAk3rman
 \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\*/
@@ -18,7 +18,7 @@ const ora = require('ora');
 const spinner = ora('');
 
 // Print header to console
-console.log(chalk.blue.bold('\nParticlecord v' + pkg.version + ' | ' + pkg.author));
+console.log(chalk.blue.bold('\nRemindcord v' + pkg.version + ' | ' + pkg.author));
 console.log(chalk.white('--> Description: ' + pkg.description));
 console.log(chalk.white('--> Github: ' + pkg.homepage + '\n'));
 
@@ -42,30 +42,6 @@ if (!config_storage.has('discord_bot_channel') || config_storage.get('discord_bo
     invalid_config = true;
 }
 
-// Config value: discord_bot_prefix
-if (!config_storage.has('discord_bot_prefix') || config_storage.get('discord_bot_prefix') === '') {
-    config_storage.set('discord_bot_prefix', '!');
-    spinner.warn('"discord_bot_prefix" value in config.json set to default: "!"');
-}
-
-// Config value: webhook_secret
-if (!config_storage.has('webhook_secret') || config_storage.get('webhook_secret') === '') {
-    let new_secret = uuidv4();
-    config_storage.set('webhook_secret', new_secret);
-    spinner.warn('"webhook_secret" value in config.json set default: "' + new_secret + '"');
-}
-
-// Config value: api_port
-if (!config_storage.has('api_port') || config_storage.get('api_port') === '') {
-    config_storage.set('api_port', 3000);
-    spinner.warn('"api_port" value in config.json set to default: "3000"');
-}
-
-// Devices config value: last_alert_device_id
-if (!devices_storage.has('last_alert_device_id') || devices_storage.get('last_alert_device_id') === '') {
-    devices_storage.set('last_alert_device_id', 'unknown');
-    spinner.warn('"last_alert_device_id" value in devices.json set to default: "unknown"');
-}
 
 // Exit if the config values are not set properly (and not in testing env)
 if (invalid_config && (process.env.testENV || process.argv[2] !== "test")) {
@@ -86,10 +62,10 @@ bot.on('ready', () => {
     bot.editStatus("online");
     // Send update to console
     spinner.succeed('Connected to Discord API');
-    spinner.succeed(`${chalk.blue.bold('Particlecord')} listening for ${chalk.yellow('API')} and ${chalk.cyan('Discord')} events`);
+    spinner.succeed(`${chalk.blue.bold('Remindcord')} listening for ${chalk.yellow('API')} and ${chalk.cyan('Discord')} events`);
     // Send connected bot message
     if (send_startup_msg) {
-        bot.createMessage(config_storage.get('discord_bot_channel'), ":white_check_mark: **Particlecord: System Online**");
+        bot.createMessage(config_storage.get('discord_bot_channel'), ":white_check_mark: **Remindcord: System Online**");
         send_startup_msg = false;
     }
 });
@@ -103,12 +79,12 @@ bot.on('messageCreate', async (msg) => {
         // Split message into components
         let parts = msg.content.split(' ');
         // Determine if we received a command
-        if (parts[0] === pre + 'help') { //  If we receive the help command, display all Particlecord commands
+        if (parts[0] === pre + 'help') { //  If we receive the help command, display all Remindcord commands
             spinner.start(`${chalk.cyan('Discord')}: Sending message to "` + pre + `help" command`);
             // Send message to Discord channel, catch error if thrown
             try {
-                await msg.channel.createMessage('**Particlecord commands**\n' +
-                    '> ' + pre + 'help: Displays all Particlecord commands\n' +
+                await msg.channel.createMessage('**Remindcord commands**\n' +
+                    '> ' + pre + 'help: Displays all Remindcord commands\n' +
                     '> ' + pre + 'status: Returns more details from the last alert\n' +
                     '> ' + pre + 'devices: Returns all known devices\n' +
                     '> ' + pre + 'details <device_id>: Returns all details for a Particle device_id\n' +
@@ -264,113 +240,3 @@ bot.on('error', err => {
     console.warn(err);
 });
 // End of Discord integration - - - - - - - - - - - - - - - - - - - - - - - - -
-
-// API Webhooks - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// Setup express
-const app = express();
-app.use(bodyParser.json());
-
-// Handle landing page
-app.get("/", (req, res) => {
-    res.redirect('https://github.com/rak3rman/particlecord');
-    spinner.info(`${chalk.yellow('API /')}: Received GET request, redirecting to Github page`);
-})
-
-// Handle post requests for Particle Tracker One events
-app.post("/api/particle/trackerone", (req, res) => {
-    spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: Received POST request, parsing data`);
-    // Make sure we are authenticated
-    if (req.body.api_key === config_storage.get('webhook_secret')) {
-        // Get device data
-        let device = devices_storage.get(req.body.coreid);
-        if (device === undefined) {
-            device = {};
-            spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) Creating new device in devices.json`);
-        }
-
-        // Make sure we have a GPS lock before updating GPS data
-        if ((JSON.parse(req.body.data).loc.lat === undefined || JSON.parse(req.body.data).loc.lon === undefined) && device !== {}) {
-            device.data.loc.cell = JSON.parse(req.body.data).loc.cell;
-            device.data.loc.batt = JSON.parse(req.body.data).loc.batt;
-            device.data.loc.temp = JSON.parse(req.body.data).loc.temp;
-            device.data.trig = JSON.parse(req.body.data).trig;
-            spinner.warn(`${chalk.yellow('API /api/particle/trackerone')}: Not updating device GPS data because there is no GPS lock`);
-        } else {
-            device.data = JSON.parse(req.body.data);
-        }
-
-        // Make sure device_name, alert_freq_min, last_alert_update exists for device
-        if (device.alert_freq_min === undefined) {
-            device.alert_freq_min = 60;
-            spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) "alert_freq_min" set to default: ` + device.alert_freq_min);
-        }
-        if (device.device_name === undefined) {
-            device.device_name = req.body.coreid;
-            spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) "device_name" set to default: ` + device.device_name);
-        }
-        if (device.last_alert_update === undefined) {
-            device.last_alert_update = moment();
-            spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) "last_alert_update" set to default: ` + device.last_alert_update);
-        }
-
-        // Determine trigger reason
-        let check_reason = "false";
-        for (let sent_reason of JSON.parse(req.body.data).trig) {
-            if (sent_reason === "radius") {
-                check_reason = " has moved past its GPS radius threshold at ";
-            } else if (sent_reason === "imu_m" || sent_reason === "img_g") {
-                check_reason = " has detected a physical motion alert at ";
-            }
-        }
-
-        // If the trigger reason is valid, check to see if we can send an alert
-        if (check_reason !== "false") {
-            // Send an alert if the freq has passed
-            if (moment().isAfter(moment(device.last_alert_update).add(device.alert_freq_min, 'm'))) {
-                device.last_alert_update = moment.unix(JSON.parse(req.body.data).time);
-                spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) Event trigger reason is valid and is past the alert frequency, sending alert`);
-                // Send message to Discord
-                let message = '@everyone ' + device.device_name + check_reason + moment.unix(JSON.parse(req.body.data).time).format('h:mm:ss a [on] MM/DD/YY Z [UTC]');
-                bot.createMessage(config_storage.get('discord_bot_channel'), message);
-                spinner.info(`${chalk.cyan('Discord')}: ${chalk.yellow('API /api/particle/trackerone')} triggered message send to channel "` + message + `"`);
-                // Prepare for status call on Discord
-                devices_storage.set('last_alert_device_id', req.body.coreid);
-            } else {
-                spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) Event trigger reason is valid but not past the alert frequency, skipping alert`);
-            }
-        } else {
-            spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) Event does not match a valid trigger reason, skipping alert`);
-        }
-
-        // Update device data
-        devices_storage.set(req.body.coreid, {
-            device_name: device.device_name,
-            data: device.data,
-            last_data_update: moment(req.body.published_at),
-            last_alert_update: device.last_alert_update,
-            alert_freq_min: device.alert_freq_min
-        });
-        spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) Updated data for device`);
-        res.status(200).end();
-    } else {
-        spinner.warn(`${chalk.yellow('API /api/particle/trackerone')}: Unauthorized POST request`);
-        res.status(403).end();
-    }
-})
-
-// Start express on defined port
-spinner.start(`${chalk.yellow('API')}: Attempting to start API http webserver`);
-app.listen(config_storage.get('api_port'), function () {
-    // Successfully started webserver
-    spinner.succeed(`${chalk.yellow('API')}: API http webserver listening on port ` + config_storage.get('api_port'));
-    // Exit if we are in testing env
-    if (process.env.testENV || process.argv[2] === "test") {
-        spinner.info(`${chalk.red('TEST MODE')}: Stopping program with exit code 0`);
-        process.exit(0);
-    } else {
-        // Start Discord bot
-        spinner.start(`${chalk.cyan('Discord')}: Attempting to connect to Discord API`);
-        bot.connect();
-    }
-})
-// End of API Webhooks - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
